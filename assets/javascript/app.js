fd38118472ab9bcadbb52dcbce7c207215cda1e1
spbryan <seanbryan@comcast.com>
2019-05-04 07:30:22 -0400

    initial check-in

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+/***********************************************
+ * Project: Rock-Paper-Scissors
+ * Developer: Sean Bryan
+ * Date: 2019-05-04
+ ***********************************************/
+
+$(document).ready(function () {
+    // Global Variables
+
+    /** On-Click for topic buttons */
+    // $(document).on("click", ".topic-button", retrieveGifs);
+
+    // $(".show-favorite-button").hide();
+    // renderButtons();
+});
